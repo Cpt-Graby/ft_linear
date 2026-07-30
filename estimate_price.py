09e1757876b71410c51be91132849dce3e9d13b7
GONELLE Alexis <alexisgonelle.mobile@gmail.com>
2026-07-31 00:41:13 +0200

    fix: optimising argparse

--- estimate_price.py
+++ estimate_price.py
@@ -1,10 +1,14 @@
 import argparse
 
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("millage", help="principale value needed", type=float)
-    parser.add_argument("theta0", type=float, help="theta0", default=0.0, action="store_true")
-    parser.add_argument("theta1", type=float, help="theta1", default=0.0, action="store_true")
+    parser.add_argument("millage", help="principale value needed",
+                        type=float)
+    parser.add_argument('-a', "--theta0", type=float, help="theta0",
+                        default=0.0, required=False)
+    parser.add_argument('-x', "--theta1", type=float, help="theta1",
+                        default=0.0)
     args = parser.parse_args()
     result = args.theta0 + args.theta1 * args.millage
     print(result)
